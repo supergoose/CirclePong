507b22b27e0b1a3128785cb1339a1ec9f48d2308
supergoose <andy@mutantlabs.com>
2015-03-30 17:22:16 +0100

    Added copyright.

--- game.js
+++ game.js
@@ -1,20 +1,29 @@
 window.onload = function() {
+
+	/*
+	First game in JS / HTML5.
+	Simple one player pong game.
+	Mostly procedural with a bit of OOP.
+	 (c) Andrew Sargeant 2015
+	 */
+
 	var canvas = document.getElementById("canvas"),
 		context = canvas.getContext("2d"),
 		width = canvas.width = window.innerWidth,
 		height = canvas.height = window.innerHeight,
 		paddle = particle.create(width / 2, height - 10, 0, 0),
-		ball = particle.create(width/2, height/2, 9, Math.random()*Math.PI*2),
-		circleRadius = ((width < height)? width : height)/3,
-		paddleWidth = 100, //should be half the circle radius
+		ball = particle.create(width/2, height/2, width/150, Math.random()*Math.PI*2),
+		circleRadius = ((width < height)? width : height)/2.5,
+		paddleWidth = circleRadius/3, //should be half the circle radius
 		paddleHeight = 8,
-		ballRadius = 5,
+		ballRadius = 8,
 		mappedAngle = 0,
 		horz = 0,
 		leftDown = false,
 		rightDown = false,
 		paddleSpeed = 20,
-		numHits = 0;
+		numHits = 0,
+		highscore = 0;
 
 	paddle.x -= paddleWidth/2;
 	paddle.y -= paddleHeight/2;
@@ -22,60 +31,46 @@ window.onload = function() {
 
 	update();
 
+	window.addEventListener("resize", onWindowResize);
+
+
+	/*** EVENTS ***/
+
+	/*
+	Window Resize
+	-------------
+	Called whenever the window size changes.
+	Resets our vars for width and height.
+	Adjusts game space
+	*/
+	function onWindowResize()
+	{
+		width = canvas.width = window.innerWidth;
+		height = canvas.height = window.innerHeight;
+		circleRadius = ((width < height)? width : height)/2.5;
+		paddleWidth = circleRadius/3;
+	}
+
+	//Two control methods - mouse or keyboard
 	document.body.addEventListener("mousemove", onMouseMove);
 	document.body.addEventListener("keydown", onKeyDown);
 	document.body.addEventListener("keyup", onKeyUp);
 
-	function reset()
-	{
-		ball.x = width/2;
-		ball.y = height/2;
-
-		paddleSpeed = 20;
-		numHits = 0;
-
-		var ballVelocity = vector.create(ball.vx, ball.vy);
-		ballVelocity.setLength(9);
-		ball.vx = ballVelocity.getX();
-		ball.vy = ballVelocity.getY();
-	}
-
-	function loadXMLDoc()
-	{
-		var xmlhttp;
-		if (window.XMLHttpRequest)
-		{// code for IE7+, Firefox, Chrome, Opera, Safari
-		  	xmlhttp=new XMLHttpRequest();
-		}
-		else
-		{// code for IE6, IE5
-		  	xmlhttp=new ActiveXObject("Microsoft.XMLHTTP");
-		}
-		xmlhttp.onreadystatechange=function()
-		{
-		  	if (xmlhttp.readyState==4 && xmlhttp.status==200)
-		    {
-		    	///document.getElementById("myDiv").innerHTML=xmlhttp.responseText;
-		    	//put the response text into a local variable
-		    }
-		}
-		xmlhttp.open("GET","ajax_info.txt",true);
-		xmlhttp.send();
-	}
-
+	/*
+	On Mouse Move
+	-------------
+	Called whenever the mouse moves.
+	Sets the var for mapped horizontal to whatever the mouseX is.
+	*/
 	function onMouseMove(event)
 	{
 
 		//get a normalised sum of mouse x value
 		horz = event.clientX;
-		
-		
-
 	}
 
 	function onKeyDown(event)
 	{
-		
 		
 		if(event.keyCode == 37)
 		{
@@ -85,8 +80,6 @@ window.onload = function() {
 		{
 			rightDown = true;
 		}
-
-		
 
 	}
 
@@ -102,6 +95,24 @@ window.onload = function() {
 		}
 
 	}
+
+	function reset()
+	{
+		ball.x = width/2;
+		ball.y = height/2;
+
+		if(numHits > highscore)
+			highscore = numHits;
+
+		numHits = 0;
+
+		var ballVelocity = vector.create(ball.vx, ball.vy);
+		ballVelocity.setLength(9);
+		ball.vx = ballVelocity.getX();
+		ball.vy = ballVelocity.getY();
+	}
+
+	
 
 	function updatePaddle()
 	{
@@ -133,11 +144,20 @@ window.onload = function() {
 
 		updatePaddle();
 
-		context.clearRect(0, 0, width, height);
 
+		collisionDetection();
+		ball.update();
+		checkEndGame();
+
+		render();
+
+		requestAnimationFrame(update);
+	}
+
+	function collisionDetection()
+	{
 		if(utils.circleInRotatedRect(ball.x, ball.y, ballRadius, paddle.x, paddle.y, paddleWidth, paddleHeight*2, -mappedAngle))
 		{
-			
 
 			//calculate the angle of incidence
 			var velocity = vector.create(ball.vx, ball.vy);
@@ -146,17 +166,19 @@ window.onload = function() {
 			var reflection = velocity.getAngle() - normalAngle + Math.PI;
 
 			velocity.setAngle(velocity.getAngle()-reflection);
-			//velocity.setLength(velocity.getLength()*1.1);
+			velocity.setLength(velocity.getLength()*1.001);
 
 			ball.vy = velocity.getY();
 			ball.vx = velocity.getX();
 
-			paddleSpeed *=1.01;
 			numHits++;
 			
 
 		}
+	}
 
+	function checkEndGame()
+	{
 		if(ball.x <= -ballRadius || ball.x >= width+ballRadius)
 		{
 			//ball.vx*=-1;
@@ -167,14 +189,30 @@ window.onload = function() {
 			//ball.vy*=-1;
 			reset();
 		}
+	}
 
+	function render()
+	{
+		context.clearRect(0, 0, width, height);
+		drawUI();
+  		drawPaddle();
+  		drawBall();
+	}
 
-		ball.update();
+	function drawUI()
+	{
+		context.font = "bold 30px Arial";
+  		context.textAlign = "center";
+  		context.fillStyle = "#bbbbbb";
+  		context.fillText(highscore, width/2, height/2 + 40);
 
-		//UI
-  		context.font = "bold 60px Times";
+		context.fillStyle = "#000000";
+  		context.font = "bold 60px Arial";
   		context.fillText(numHits, width/2, height/2);
+	}
 
+	function drawPaddle()
+	{
 		//draw the players paddle
 		context.save()
 		context.translate(paddle.x, paddle.y);
@@ -185,13 +223,13 @@ window.onload = function() {
       	context.fill();
 
 		context.restore();
-		
+	}
+
+	function drawBall()
+	{
 		context.beginPath();
 		context.arc(ball.x, ball.y, ballRadius, 0, Math.PI * 2, false);
 		context.fill();
-
-
-		requestAnimationFrame(update);
 	}
 
 }
